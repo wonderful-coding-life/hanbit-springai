fb975e6ff5d48eccd12708b26b2a9c1ad1d1eccf
spiderman <spiderman@xhift.co.kr>
2025-11-12 21:53:21 +0900

    updated audio model

--- openai-audio/src/test/java/com/example/demo/OpenAiAudioSpeechModelTests.java
+++ openai-audio/src/test/java/com/example/demo/OpenAiAudioSpeechModelTests.java
@@ -25,20 +25,30 @@ public class OpenAiAudioSpeechModelTests {
 
     @Test
     public void testSpeechModelSimple() throws IOException {
-        byte[] bin = speechModel.call("안녕하세요 반갑습니다. 스프링부트는 자바 프레임워크 중에 가장 인기가 많은 프레임워크입니다.");
+        byte[] bin = speechModel.call("이번역은 선릉, 선릉역입니다. 내리실 문은 오른쪽 입니다. 멀티캠퍼스로 가실 분들은 이번역에서 내리시기 바랍니다.");
         Files.write(Paths.get("D:\\archive\\audio\\ai_tts_simple.mp3"), bin);
     }
 
     @Test
     public void testSpeechModelOptions() throws IOException {
         OpenAiAudioSpeechOptions speechOptions = OpenAiAudioSpeechOptions.builder()
-                .model("tts-1") // tts-1, tts-1-hd, gpt-4o-mini-tts (not ready yet for spring ai)
+                .model("tts-1-hd") // tts-1, tts-1-hd, gpt-4o-mini-tts (not ready yet for spring ai)
                 .voice(OpenAiAudioApi.SpeechRequest.Voice.NOVA) // default ALLOY?
                 .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
                 .speed(1.0f) // fast > 1.0
                 .build();
 
-        String text = "안녕하세요 반갑습니다. 스프링부트는 자바 프레임워크 중에 가장 인기가 많은 프레임워크입니다.";
+        String text = """
+                안녕하세요, 고객님.
+                문의 사항이 있으시면 '삐' 소리 후에 음성으로 남겨 주세요.
+                확인 후 빠르게 연락드리겠습니다.
+                """;
+
+        String textEng = """
+                Hello, this is Multicampus.
+                If you have any questions, please leave a voice message after the beep.
+                Thank you!
+                """;
 
         SpeechPrompt speechPrompt = new SpeechPrompt(text, speechOptions);
         SpeechResponse response = speechModel.call(speechPrompt);
--- openai-audio/src/test/java/com/example/demo/OpenAiAudioTranscriptionModelTests.java
+++ openai-audio/src/test/java/com/example/demo/OpenAiAudioTranscriptionModelTests.java
@@ -24,10 +24,8 @@ public class OpenAiAudioTranscriptionModelTests {
     @Test
     public void testTranscriptModelSimple() {
         Resource resource = new ClassPathResource("/audio/voc_kart_rider.mp3");
-        //Resource resource = new ClassPathResource("/audio/sample_audio.mp3");
-        //Resource resource = new ClassPathResource("/image/Disney_World_2.jpg");
-        //Resource resource = new FileSystemResource("D:\\archive\\audio\\sample_audio.mpg");
-        //Resource resource = new UrlResource("https://xxx/sample_audio.mpg");
+        //Resource resource = new FileSystemResource("D:\\archive\\audio\\voc_kart_rider.mpg");
+        //Resource resource = new UrlResource("https://xxx/sample_audio.mp3");
         String script = transcriptionModel.call(resource);
         log.info("script {}", script);
     }
